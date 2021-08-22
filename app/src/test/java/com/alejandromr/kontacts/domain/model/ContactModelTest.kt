e0ff78f099d82a723c918c98f56b856f777cf07e
Alejandro Muñoz <alejandromr@visual-engin.com>
2021-08-22 13:30:51 +0200

    Added missing model tests.

--- app/src/test/java/com/alejandromr/kontacts/domain/model/ContactModelTest.kt
+++ app/src/test/java/com/alejandromr/kontacts/domain/model/ContactModelTest.kt
@@ -1,3 +1,41 @@
 package com.alejandromr.kontacts.domain.model
 
-class ContactModelTest
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+
+class ContactModelTest {
+    @Test
+    fun `initializes as expected with parameters`() {
+        val nameModel = NameModel("name", "surname")
+        val streetModel = StreetModel("name", "66")
+        val locationModel = LocationModel(streetModel, "city", "state")
+        val pictureModel = PictureModel("large", "medium", "thumbnail")
+        val registrationModel = RegistrationModel("date")
+        val model = ContactModel(
+            nameModel,
+            "male",
+            locationModel,
+            registrationModel,
+            "phone",
+            "mail",
+            pictureModel
+        )
+
+        with(model) {
+            assertEquals("name", name.first)
+            assertEquals("surname", name.last)
+            assertEquals("name", location.street.name)
+            assertEquals("66", location.street.number)
+            assertEquals("city", location.city)
+            assertEquals("state", location.state)
+            assertEquals("state", location.state)
+            assertEquals("large", picture.large)
+            assertEquals("medium", picture.medium)
+            assertEquals("thumbnail", picture.thumbnail)
+            assertEquals("date", registered.date)
+            assertEquals("male", gender)
+            assertEquals("phone", phone)
+            assertEquals("mail", email)
+        }
+    }
+}
--- app/src/test/java/com/alejandromr/kontacts/domain/model/LocationModelTest.kt
+++ app/src/test/java/com/alejandromr/kontacts/domain/model/LocationModelTest.kt
@@ -1,3 +1,19 @@
 package com.alejandromr.kontacts.domain.model
 
-class LocationModelTest
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+
+class LocationModelTest {
+    @Test
+    fun `initializes as expected with parameters`() {
+        val streetModel = StreetModel("name", "66")
+        val model = LocationModel(streetModel, "city", "state")
+
+        with(model) {
+            assertEquals("name", street.name)
+            assertEquals("66", street.number)
+            assertEquals("city", city)
+            assertEquals("state", state)
+        }
+    }
+}
